31c0340baf2771a57fd431d6d8bce3a063945edd
Antony Jones <aj@porsh.net>
2018-02-02 16:40:21 +0000

    node lint config

---
+++ config.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const { error, never, off, single } = require('./constants')
+
+module.exports = {
+
+  extends: 'eslint:recommended',
+
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+
+  globals: {
+    after: true,
+    afterEach: true,
+    artifacts: true,
+    before: true,
+    beforeEach: true,
+    console: true,
+    contract: true,
+    describe: true,
+    document: true,
+    expect: true,
+    fetch: true,
+    it: true,
+    test: true,
+    navigator: true,
+    process: true,
+    URL: true,
+    window: true
+  },
+
+  rules: {
+    'no-console': off,
+    'comma-dangle': [ 1, never ],
+    'max-len': off,
+    'no-mixed-spaces-and-tabs': error,
+    'no-tabs': error,
+    'no-use-before-define': [ error, { functions: false, classes: true } ],
+    'no-undefined': off,
+    'object-curly-spacing': [ 0, never ],
+    'semi': [ error, never ],
+    'space-before-function-paren': 0,
+    'quotes': [ error, single ]
+  }
+}
---
+++ constants.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+  never: 'never',
+  off: 0,
+  warn: 1,
+  error: 2,
+  single: 'single'
+}
