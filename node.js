6b9bee1aaae358d8bf7f7415f1a9d90c1b61b478
Mirek Rusin <mirek@me.com>
2018-06-25 17:16:33 +0100

    The 'parserOptions.ecmaFeatures.experimentalObjectRestSpread' option...
    ... is deprecated.

--- node.js
+++ node.js
@@ -7,11 +7,8 @@ module.exports = {
   extends: 'eslint:recommended',
 
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
 
   env: {
