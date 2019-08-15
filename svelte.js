633bfbab43084eff721db42c41d8eb7529d5b217
Antony Jones <aj@desirableobjects.co.uk>
2019-08-15 12:21:55 +0100

    disable conflicting import rules for Svelte

--- svelte.js
+++ svelte.js
@@ -4,5 +4,16 @@ module.exports = {
   extends: [
     '@beyonk/eslint-config',
     'svelte3'
+  ],
+  'overrides': [
+    {
+      'files': ['*.svelte','*.html'],
+      'rules': {
+        'import/first': 'off',
+        'import/no-duplicates': 'off',
+        'import/no-mutable-exports': 'off',
+        'import/no-unresolved': 'off'
+      }
+    }
   ]
 }
