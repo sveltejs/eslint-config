afcc7b74e5b0765d047746d6b668ce8d39f24347
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-02-04 18:43:06 -0300

    prefer method declaration syntax

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ module.exports = {
 		'@typescript-eslint/explicit-function-return-type': off,
 		'@typescript-eslint/explicit-member-accessibility': off,
 		'@typescript-eslint/explicit-module-boundary-types': off,
+		'@typescript-eslint/method-signature-style': [error, "method"],
 		'@typescript-eslint/naming-convention': off,
 		'@typescript-eslint/no-empty-function': off,
 		'@typescript-eslint/no-explicit-any': off,
