055510804f84e508bff702a82c5e2738a1aa3211
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-05-18 10:57:49 -0700

    Simplify config and remove style rules

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
 'use strict';
 
-const { off, all, always, error, never, warn } = require('./constants');
-
 module.exports = {
 	rules: {
-		'arrow-spacing': 2,
-		'brace-style': 2,
-		camelcase: off,
-		'comma-dangle': [ error, never ],
-		curly: [ error, 'multi-line' ],
-		'eol-last': error,
-		'import/no-unresolved': off,
-		'keyword-spacing': [ 2, { before: true, after: true } ],
-		'no-unused-vars': off,
-		'no-mixed-spaces-and-tabs': [ 2, 'smart-tabs' ],
-		'no-cond-assign': 0,
-		'no-class-assign': 2,
-		'no-const-assign': 2,
-		'no-import-assign': warn,
-		'no-inner-declarations': 0,
-		'no-this-before-super': 2,
-		'no-var': 2,
-		'no-unreachable': 2,
-		'object-shorthand': [ 2, always ],
-		'one-var': off,
-		'prefer-arrow-callback': 2,
-		'prefer-const': [ 2, { destructuring: all } ],
-		quotes: ['error', 'single', { avoidEscape: true }],
-		'quote-props': [ 2, 'as-needed' ],
-		'require-atomic-updates': off,
-		semi: [ 2, always ],
-		'space-before-blocks': [ 2, always ],
-		'valid-typeof': 2,
-		'@typescript-eslint/array-type': [ error, { default: 'array-simple' } ],
-		'@typescript-eslint/ban-ts-comment': off,
-		'@typescript-eslint/ban-types': off,
-		'@typescript-eslint/camelcase': off,
-		'@typescript-eslint/class-name-casing': off,
-		'@typescript-eslint/explicit-function-return-type': off,
-		'@typescript-eslint/explicit-member-accessibility': off,
-		'@typescript-eslint/explicit-module-boundary-types': off,
-		'@typescript-eslint/naming-convention': off,
-		'@typescript-eslint/no-empty-function': off,
-		'@typescript-eslint/no-explicit-any': off,
-		'@typescript-eslint/no-inferrable-types': off,
-		'@typescript-eslint/no-object-literal-type-assertion': off,
+		'@typescript-eslint/array-type': [ 'error', { default: 'array-simple' } ],
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/camelcase': 'off',
+		'@typescript-eslint/class-name-casing': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-member-accessibility': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/no-object-literal-type-assertion': 'off',
 		'@typescript-eslint/no-unused-vars': ['error', { args: 'after-used', argsIgnorePattern: '^_' }],
-		'@typescript-eslint/no-use-before-define': off,
-		'@typescript-eslint/no-this-alias': off,
-		'@typescript-eslint/prefer-interface': off,
-		'@typescript-eslint/space-infix-ops': 'error'
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/no-this-alias': 'off',
+		'@typescript-eslint/prefer-interface': 'off',
+		'import/no-unresolved': 'off',
+		'no-constant-condition': ['error', { checkLoops: false }],
+		'no-empty': ['error', { allowEmptyCatch: true }],
+		'no-inner-declarations': 'off',
+		'no-sparse-arrays': 'off',
+		'no-restricted-properties': [
+			'error',
+			{ 'object': 'test', 'property': 'only', 'message': 'Do not check in test.only tests.' }
+		],
+		'no-var': 'error',
+		'object-shorthand': [ 'error', 'always' ],
+		'prefer-arrow-callback': 'error',
+		'prefer-const': [ 'error', { destructuring: 'all' } ],
+		'quotes': ['error', 'single', { avoidEscape: true }],
+		'quote-props': [ 'error', 'as-needed' ],
+		'svelte/no-inner-declarations': 'off',
+		'unicorn/prefer-node-protocol': 'error'
 	},
 	globals: {
 		globalThis: false
@@ -57,31 +43,20 @@ module.exports = {
 	env: {
 		es6: true,
 		browser: true,
-		node: true,
-		mocha: true
+		node: true
 	},
 	extends: [
 		'eslint:recommended',
 		'plugin:import/errors',
 		'plugin:import/warnings',
 		'plugin:import/typescript',
-		'plugin:@typescript-eslint/recommended'
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
 	],
 	parserOptions: {
-		ecmaVersion: 9,
+		ecmaVersion: 13, // es2022
 		sourceType: 'module'
 	},
-	plugins: ['svelte3'],
-	overrides: [
-		{
-			files: ['*.js'],
-			rules: {
-				'@typescript-eslint/no-var-requires': off
-			}
-		},
-		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3'
-		}
-	]
+	plugins: ['unicorn']
 };
