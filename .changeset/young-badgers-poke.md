ea485ee9e5dd474c00e2b4ad7a9b82f2568e5815
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-07-16 11:45:33 -0700

    fix: enable projectService (#47)

---
+++ .changeset/young-badgers-poke.md
@@ -0,0 +1,5 @@
+---
+"@sveltejs/eslint-config": patch
+---
+
+fix: enable projectService
--- index.js
+++ index.js
@@ -31,6 +31,9 @@ export default [
 			globals: {
 				...globals.browser,
 				...globals.node
+			},
+			parserOptions: {
+				projectService: true
 			}
 		},
 		rules: {
